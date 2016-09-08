60fe6b7de51663e107ba7d28274a48db2e78f6ac
enoch232 <enoch232@gmail.com>
2016-09-08 10:36:58 -0700

    add password confirmation, and redirect to a right path

--- reactnativedemo/app/controllers/users_controller.rb
+++ reactnativedemo/app/controllers/users_controller.rb
@@ -1,39 +1,41 @@
-module API
-	class UsersController < ApplicationController
 
-	  def index
-	  	@users = User.all
-	  	respond_to do |format|
-	  		format.json
+class UsersController < ApplicationController
+
+  def index
+  	@users = User.all
+  	respond_to do |format|
+  		format.html
+  		format.json{render json: @users}
+  	end
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+  	@user.access_token = ApiKey.create.generate_key
+  	if @user.save
+  		respond_to do |format|
+        format.html{redirect_to users_path}
+	  		format.json{render json: @user}
 	  	end
-	  end
-
-	  def new
-	  	@user = User.new
-	  end
-
-	  def create
-	  	@user = User.new(params[:users])
-	  	@user.access_token = ApiKey.create.access_token
-	  	if @user.save
-	  		respond_to do |format|
-		  		format.json
-		  	end
-	  	else
-	  		respond_to do |format|
-		  		format.json
-		  	end
+  	else
+  		respond_to do |format|
+        format.html{redirect_to :index}
+	  		format.json{render json: @user.errors}
 	  	end
-	  end
+  	end
+  end
 
-	  def edit
-	  end
+  def edit
+  end
 
-	  def update
-	  end
+  def update
+  end
 
-	  def destroy
-	  end
+  def destroy
+  end
 
-	end
 end
--- reactnativedemo/app/models/user.rb
+++ reactnativedemo/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :posts
+  attr_accessor :password, :password_confirmation
 end

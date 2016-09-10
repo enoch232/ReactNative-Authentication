e4eb568a28a5ea23f9b5094d2af873b66ba4588f
enoch232 <enoch232@gmail.com>
2016-09-09 23:45:21 -0700

    add initial start

--- reactlogin/index.ios.js
+++ reactlogin/index.ios.js
@@ -9,45 +9,44 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
 
 class reactlogin extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <View style = {styles.container}>
+        <Text style={styles.title}>
+          React Login Apps
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+        <Text>
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
       </View>
     );
   }
-}
+  renderLogin(){
+    return(
+      <TouchableHighlight>
+      Hello
+      </TouchableHighlight>
 
+    );
+  }
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  container:{
+    flex:1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  title:{
+    fontSize: 30,
+    textAlign: "center",
+    alignItems: "flex-end",
+    top:30
+  }
+
 });
+
 
 AppRegistry.registerComponent('reactlogin', () => reactlogin);

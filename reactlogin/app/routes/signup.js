9798218542a32d25bf6e19a9a3d5884e7fb9aba2
enoch232 <enoch232@gmail.com>
2016-09-11 13:58:31 -0700

    add text input

--- reactlogin/app/routes/signup.js
+++ reactlogin/app/routes/signup.js
@@ -9,9 +9,55 @@ import {
 export default class SignUpPage extends Component{
 	render(){
 		return(
-			<View>
-				<Text>SignUp</Text>
+			<View style = {styles.container}>
+				<Text>Sign Up</Text>
+				<Text style = {styles.label}>Name: </Text>
+				<TextInput style = {styles.nameTextInput} />
+				<Text style = {styles.label}>Email: </Text>
+				<TextInput style = {styles.emailTextInput} />
+				<Text style = {styles.label}>Password: </Text>
+				<TextInput style = {styles.passwordTextInput} />
+				<Text style = {styles.label}>Password Confirmation: </Text>
+				<TextInput style = {styles.passwordConfirmationTextInput} />
 			</View>
 		)
 	}
+}
+const styles = {
+	container:{
+		flex: 1,
+		left: 30,
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "flex-start"
+	},
+	label:{
+		textAlign:"left"
+	},
+	nameTextInput: {
+		height:40,
+		borderColor: "gray",
+		borderWidth: 1,
+		width:200
+	},
+	emailTextInput: {
+		height:40,
+		borderColor: "gray",
+		borderWidth: 1,
+		width:200
+	},
+	passwordTextInput: {
+		height:40,
+		borderColor: "gray",
+		borderWidth: 1,
+		width:200
+	},
+	passwordConfirmationTextInput: {
+		height:40,
+		borderColor: "gray",
+		borderWidth: 1,
+		width:200
+	}
+
+
 }

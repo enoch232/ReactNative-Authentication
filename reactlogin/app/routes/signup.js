f2e9715c6f728178a3126ef3a7d374033b193324
enoch232 <enoch232@gmail.com>
2016-09-14 16:44:04 -0700

    add input for name, email, password, and password confirmation

--- reactlogin/app/routes/signup.js
+++ reactlogin/app/routes/signup.js
@@ -3,22 +3,36 @@ import {
 	Navigator,
 	Text,
 	TextInput,
-	View
+	View,
+	TouchableOpacity
 
 }from "react-native";
 export default class SignUpPage extends Component{
+	constructor(props){
+		super(props)
+		this.state = {name:"name", email: "email", password:"123456", passwordConfirmation: "123456"}
+	}
+	_signupPress(name){
+
+	}
 	render(){
 		return(
-			<View style = {styles.container}>
-				<Text>Sign Up</Text>
-				<Text style = {styles.label}>Name: </Text>
-				<TextInput style = {styles.nameTextInput} />
-				<Text style = {styles.label}>Email: </Text>
-				<TextInput style = {styles.emailTextInput} />
-				<Text style = {styles.label}>Password: </Text>
-				<TextInput style = {styles.passwordTextInput} />
-				<Text style = {styles.label}>Password Confirmation: </Text>
-				<TextInput style = {styles.passwordConfirmationTextInput} />
+			<View>
+				<View>
+					<Text>Header</Text>
+				</View>
+				<View style = {styles.container}>
+					<Text>Sign Up</Text>
+					<Text style = {styles.label} >Name: </Text>
+					<TextInput style = {styles.nameTextInput} value={this.state.name} onChangeText = {(name) => this.setState({name})}/>
+					<Text style = {styles.label}>Email: </Text>
+					<TextInput style = {styles.emailTextInput} value = {this.state.email} onChangeText = {(email) => this.setState({email})}/>
+					<Text style = {styles.label}>Password: </Text>
+					<TextInput style = {styles.passwordTextInput} secureTextEntry={true} value = {this.state.password} onChangeText = {(password) => this.setState({password})}/>
+					<Text style = {styles.label}>Password Confirmation: </Text>
+					<TextInput style = {styles.passwordConfirmationTextInput} secureTextEntry = {true} value = {this.state.passwordConfirmation} onChangeText = {(passwordC)=> this.setState(passwordC)}/>
+					<TouchableOpacity><Text>Sign Up</Text></TouchableOpacity>
+				</View>
 			</View>
 		)
 	}
@@ -27,6 +41,7 @@ const styles = {
 	container:{
 		flex: 1,
 		left: 30,
+		top: 80,
 		flexDirection: "column",
 		justifyContent: "center",
 		alignItems: "flex-start"

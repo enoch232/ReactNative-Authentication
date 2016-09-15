819b1372f3782b4fed0576a6ea96d85343dedfb7
enoch232 <enoch232@gmail.com>
2016-09-15 00:41:06 -0700

    remove forgery from api, and add sign up fetch

--- reactlogin/app/routes/signup.js
+++ reactlogin/app/routes/signup.js
@@ -10,10 +10,29 @@ import {
 export default class SignUpPage extends Component{
 	constructor(props){
 		super(props)
-		this.state = {name:"name", email: "email", password:"123456", passwordConfirmation: "123456"}
+		this.state = {name:"", email: "", password:"", passwordConfirmation: ""}
 	}
-	_signupPress(name){
-
+	async _signupPress(){
+		try{
+			let response = await fetch('http://localhost:3000/users', {
+			  method: 'POST',
+			  headers: {
+			    'Accept': 'application/json',
+			    'Content-Type': 'application/json',
+			  },
+			  body: JSON.stringify({
+			  	user:{
+			  		name: this.state.name,
+				    email: this.state.email,
+				    password: this.state.password,
+				    password_confirmation: this.state.passwordConfirmation
+			  	}
+			  })
+			})
+			this.setState({response: "registered!"})
+		} catch(error){
+			console.error(error)
+		}
 	}
 	render(){
 		return(
@@ -22,6 +41,7 @@ export default class SignUpPage extends Component{
 					<Text style = {{fontSize: 30}}>Sign Up</Text>
 				</View>
 				<View style = {styles.inputContainer}>
+					<Text style = {styles.label} >{this.state.response}</Text>
 					<Text style = {styles.label} >Name: </Text>
 					<TextInput style = {styles.nameTextInput} value={this.state.name} onChangeText = {(name) => this.setState({name})}/>
 					<Text style = {styles.label}>Email: </Text>
@@ -29,8 +49,8 @@ export default class SignUpPage extends Component{
 					<Text style = {styles.label}>Password: </Text>
 					<TextInput style = {styles.passwordTextInput} secureTextEntry={true} value = {this.state.password} onChangeText = {(password) => this.setState({password})}/>
 					<Text style = {styles.label}>Password Confirmation: </Text>
-					<TextInput style = {styles.passwordConfirmationTextInput} secureTextEntry = {true} value = {this.state.passwordConfirmation} onChangeText = {(passwordC)=> this.setState(passwordC)}/>
-					<TouchableOpacity><Text>Sign Up</Text></TouchableOpacity>
+					<TextInput style = {styles.passwordConfirmationTextInput} secureTextEntry = {true} value = {this.state.passwordConfirmation} onChangeText = {(passwordConfirmation)=> this.setState({passwordConfirmation})}/>
+					<TouchableOpacity onPress = {this._signupPress.bind(this)}><Text>Sign Up</Text></TouchableOpacity>
 				</View>
 			</View>
 		)

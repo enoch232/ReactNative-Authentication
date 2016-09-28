d3849e3eb314946531afa596cc3313ccca1fbb72
enoch232 <enoch232@gmail.com>
2016-09-28 15:55:21 -0700

    support for android initialize

--- reactlogin/index.android.js
+++ reactlogin/index.android.js
@@ -8,46 +8,40 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Navigator,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
+import RootPage from "./app/routes/root";
+import SignInPage from "./app/routes/signin";
+import SignUpPage from "./app/routes/signup";
+import DashboardPage from "./app/routes/dashboard";
 
 class reactlogin extends Component {
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return(
+      <Navigator 
+        initialRoute = {{title: "Home"}}
+        renderScene = {function(route, navigator){
+          if (route.title == "Home"){
+            return <RootPage navigator = {navigator}/>
+          }
+          if (route.title == "SignIn"){
+            return <SignInPage navigator = {navigator}/>
+          }
+          if (route.title == "SignUp"){
+            return <SignUpPage navigator = {navigator}/>
+          }
+          if (route.title = "Dashboard"){
+            return <DashboardPage navigator = {navigator}/>
+          }
+        }}
+      >
+      </Navigator>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('reactlogin', () => reactlogin);
